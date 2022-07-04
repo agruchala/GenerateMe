8642cd2d859e7865ab5a1d5643a1db3fe9d3082c
Artur Gruchała <gruchala.a@gmail.com>
2022-07-04 22:48:09 +0200

    Initial setup

---
+++ GenerateMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenerateMe
+//
+//  Created by Artur Gruchała on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
