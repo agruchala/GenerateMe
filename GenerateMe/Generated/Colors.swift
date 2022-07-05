ee80483c44adbd6baf376628eb00916d4bd4fe6b
Artur Gruchała <gruchala.a@gmail.com>
2022-07-05 22:24:02 +0200

    Final setup

---
+++ GenerateMe/Generated/Colors.swift
@@ -0,0 +1,18 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import UIKit.UIColor
+
+typealias Color = UIColor
+
+// MARK: - Colors
+
+extension Color {
+    static let action = Color(named: "Action")!
+    static let background = Color(named: "Background")!
+    static let destructive = Color(named: "Destructive")!
+    static let onBackground = Color(named: "OnBackground")!
+    static let text = Color(named: "Text")!
+}
+
+// swiftlint:enable all
